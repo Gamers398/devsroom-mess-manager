5676b227bcf87e56c37d108d290f2505fc7f0dad
Iftakhar Ahmed <gamersgo398@gmail.com>
2026-08-08 19:26:26 +0600

    Point SQLite backup dump_binary_path to /usr/bin

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'dump' => [
-                'dump_binary_path' => '/root/.nix-profile/bin',
+                'dump_binary_path' => '/usr/bin',
             ],
             'busy_timeout' => null,
             'journal_mode' => null,
